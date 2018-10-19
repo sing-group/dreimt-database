d8d5a8ec4fc0f40a80bca2a3a180141706362462
Hugo López-Fernández <hlfernandez@uvigo.es>
2018-10-19 11:25:29 +0200

    Fixes signature processing to deal with NA PubMedIDs
    Fixes the script so that when a PubMedID is NA then the value is not inserted in
    the database and it remains empty.

--- process_signatures.sh
+++ process_signatures.sh
@@ -4,7 +4,11 @@ cat $1 | awk -F'\t' '
     NR>1 {
         gsub(" ", "_", $8);
         printf "INSERT IGNORE INTO drug (commonName, sourceName, sourceDb) VALUES (\"%s\", \"%s\", \"%s\");\n", $9, $10, $11;
-        printf "INSERT IGNORE INTO signature (signatureName, cellTypeA, cellTypeB, article_pubmedId, sourceDb, experimentalDesign, organism, disease) VALUES (\"%s\", \"%s\", \"%s\", %s, \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $4, $5, $15, $3, toupper($8), $6, $7;
+        if($15 == "NA") {
+            printf "INSERT IGNORE INTO signature (signatureName, cellTypeA, cellTypeB, sourceDb, experimentalDesign, organism, disease) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $4, $5, $3, toupper($8), $6, $7;
+        } else {
+            printf "INSERT IGNORE INTO signature (signatureName, cellTypeA, cellTypeB, article_pubmedId, sourceDb, experimentalDesign, organism, disease) VALUES (\"%s\", \"%s\", \"%s\", %s, \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $4, $5, $15, $3, toupper($8), $6, $7;
+        }
         printf "INSERT INTO drug_signature_interaction (drug_sourceName, drug_sourceDb, signature, tes, pValue, fdr) VALUES (\"%s\", \"%s\", \"%s\", %s, %s, %s);\n", $10, $11, $1, $12, $13, $14;
     }
 ';
