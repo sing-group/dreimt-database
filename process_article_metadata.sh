7ba0cb92ddddd892d0bf996f2af159d1b966004c
Hugo López-Fernández <hlfernandez@uvigo.es>
2018-10-19 10:48:58 +0200

    Fixes article metadata processing
    Fixes article metadata processing so that single quotes inside authors, article
    title or article abstract are escaped.

--- process_article_metadata.sh
+++ process_article_metadata.sh
@@ -2,6 +2,11 @@
 
 cat $1 | awk -F'\t' '
     NR>1 {
-        printf "INSERT INTO article_metadata (pubmedId, title, authors, articleAbstract) VALUES (%s, \"%s\", \"%s\", \"%s\");\n", $1, $2, $3, $4;
+        # Escape single quotes (\x27) so that the MySQL script works properly
+        gsub("\x27", "\\\x27",$2)
+        gsub("\x27", "\\\x27",$3)
+        gsub("\x27", "\\\x27",$4)
+
+        printf "INSERT INTO article_metadata (pubmedId, title, authors, articleAbstract) VALUES (%s, \x27%s\x27, \x27%s\x27, \x27%s\x27);\n", $1, $2, $3, $4;
     }
 ';
