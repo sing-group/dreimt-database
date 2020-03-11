bcbccc65132c8ea7c3a593dbf72bb9a4db9ca244
Hugo López-Fernández <hlfernandez@uvigo.es>
2020-03-11 12:32:57 +0100

    Adapt drug script to process new fields (MOA and status)

--- process_drug.sh
+++ process_drug.sh
@@ -2,13 +2,14 @@
 
 cat $1 | awk -F'\t' '
 	BEGIN { 
-		printf "INSERT INTO drug (id, commonName, sourceDb, sourceName) VALUES" 
+		printf "INSERT INTO drug (id, commonName, sourceDb, sourceName, status, moa) VALUES"
 	}
 	NR>2 { 
 		printf ","; 
 	}
 	NR>1 {
-		printf "\n  (%s, \"%s\", \"%s\", \"%s\")", $1, $3, $4, $5;
+		status = toupper($10);
+		printf "\n  (%s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")", $1, $3, $4, $5, status, $9;
 	}
 	END { printf ";\n"}
 ';
