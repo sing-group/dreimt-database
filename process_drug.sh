0b28b311958061d1009c441f194f92368f918065
Hugo López-Fernández <hlfernandez@uvigo.es>
2020-04-21 15:25:03 +0200

    Adapt script to generate a table for drug MOA

--- process_drug.sh
+++ process_drug.sh
@@ -2,14 +2,36 @@
 
 cat $1 | awk -F'\t' '
 	BEGIN { 
-		printf "INSERT INTO drug (id, commonName, sourceDb, sourceName, status, moa) VALUES"
+		printf "INSERT INTO drug (id, commonName, sourceDb, sourceName, status) VALUES"
 	}
 	NR>2 { 
 		printf ","; 
 	}
 	NR>1 {
 		status = toupper($10);
-		printf "\n  (%s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")", $1, $3, $4, $5, status, $9;
+		printf "\n  (%s, \"%s\", \"%s\", \"%s\", \"%s\")", $1, $3, $4, $5, status;
+	}
+	END { printf ";\n\n"}
+';
+
+cat $1 | awk -F'\t' '
+	BEGIN {
+		printf "INSERT IGNORE INTO drug_moa (id, moa) VALUES"
+	}
+	NR>2 {
+		printf ",";
+	}
+	NR>1 {
+		moaCount = split($9, moa, ",");
+		for(i=0; ++i <= moaCount;) {
+			if(moa[i] != "") {
+				gsub(/^[[:space:]]+|[[:space:]]+$/,"",moa[i]);
+				printf "\n  (%s, \"%s\")", $1, moa[i];
+				if(i < moaCount) {
+					printf ",";
+				}
+			}
+		}
 	}
 	END { printf ";\n"}
 ';
