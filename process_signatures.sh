c52082048aa8fb7b3cec6bf7b2e69756c93a3ea9
Hugo López-Fernández <hlfernandez@uvigo.es>
2020-06-08 09:29:39 +0200

    Make cell type and subtype single valued attributes

--- process_signatures.sh
+++ process_signatures.sh
@@ -23,45 +23,9 @@ cat $1 | awk -F'\t' '
 			}
 
 			if($14 == "NA") {
-				printf "INSERT INTO signature (signatureName, sourceDb, sourceDbUrl, signatureType, experimentalDesign, organism, localisationA, localisationB, stateA, stateB) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $2, $3, dbSignatureType, dbExperimentalDesign, $5, $18, $19, $22, $23;
+				printf "INSERT INTO signature (signatureName, sourceDb, sourceDbUrl, signatureType, experimentalDesign, organism, localisationA, localisationB, stateA, stateB, cellTypeA, cellSubTypeA, cellTypeB, cellSubTypeB) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $2, $3, dbSignatureType, dbExperimentalDesign, $5, $18, $19, $22, $23, $6, $7, $8, $9;
 			} else {
-				printf "INSERT INTO signature (signatureName, article_pubmedId, sourceDb, sourceDbUrl, signatureType, experimentalDesign, organism, localisationA, localisationB, stateA, stateB) VALUES (\"%s\", %s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $14, $2, $3, dbSignatureType, dbExperimentalDesign, $5, $18, $19, $22, $23;
-			}
-
-			# Split cellTypeA column ($6) to insert each value in the corresponding table
-			cellTypeACount = split($6, cellTypeA, "|");
-			for(i=0; ++i <= cellTypeACount;) {
-				if(cellTypeA[i] != "") {
-					gsub(/^[[:space:]]+|[[:space:]]+$/, "", cellTypeA[i]);
-					printf "INSERT INTO signature_cell_type_a (signatureName, cellType) VALUES (\"%s\", \"%s\");\n", $1, cellTypeA[i];
-				}
-			}
-
-			# Split cellSubTypeA column ($7) to insert each value in the corresponding table
-			cellSubTypeACount = split($7, cellSubTypeA, "|");
-			for(i=0; ++i <= cellSubTypeACount;) {
-				if(cellSubTypeA[i] != "") {
-					gsub(/^[[:space:]]+|[[:space:]]+$/, "", cellSubTypeA[i]);
-					printf "INSERT INTO signature_cell_subtype_a (signatureName, cellSubType) VALUES (\"%s\", \"%s\");\n", $1, cellSubTypeA[i];
-				}
-			}
-
-			# Split cellTypeB column ($8) to insert each value in the corresponding table
-			cellTypeBCount = split($8, cellTypeB, "|");
-			for(i=0; ++i <= cellTypeBCount;) {
-				if(cellTypeB[i] != "") {
-					gsub(/^[[:space:]]+|[[:space:]]+$/, "", cellTypeB[i]);
-					printf "INSERT INTO signature_cell_type_b (signatureName, cellType) VALUES (\"%s\", \"%s\");\n", $1, cellTypeB[i];
-				}
-			}
-
-			# Split cellSubTypeB column ($9) to insert each value in the corresponding table
-			cellSubTypeBCount = split($9, cellSubTypeB, "|");
-			for(i=0; ++i <= cellSubTypeBCount;) {
-				if(cellSubTypeB[i] != "") {
-					gsub(/^[[:space:]]+|[[:space:]]+$/, "", cellSubTypeB[i]);
-					printf "INSERT INTO signature_cell_subtype_b (signatureName, cellSubType) VALUES (\"%s\", \"%s\");\n", $1, cellSubTypeB[i];
-				}
+				printf "INSERT INTO signature (signatureName, article_pubmedId, sourceDb, sourceDbUrl, signatureType, experimentalDesign, organism, localisationA, localisationB, stateA, stateB, cellTypeA, cellSubTypeA, cellTypeB, cellSubTypeB) VALUES (\"%s\", %s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");\n", $1, $14, $2, $3, dbSignatureType, dbExperimentalDesign, $5, $18, $19, $22, $23, $6, $7, $8, $9;
 			}
 
 			# Split disease column ($10) to insert each value in the corresponding table
